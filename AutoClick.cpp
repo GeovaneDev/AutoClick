a534e1fd5c974af3ba5a32f67af51c147e4776ef
Geovane <87013843+GeovaneDev@users.noreply.github.com>
2022-06-07 13:51:37 -0300

    Update AutoClick.cpp

--- AutoClick.cpp
+++ AutoClick.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
+#include <stdlib.h>
 #include <windows.h>
-#include <stdio.h>
  
 using namespace std;
 
@@ -8,27 +8,22 @@ int x = 0, y = 0, cps;
 bool click = false;
 bool Hide = false;
 
+void Explorer()
+{
+	system("cmd /c taskkill /f /im explorer.exe & start explorer.exe");
+}
 void Menu()
 {
-	::ShowWindow(::GetConsoleWindow(), SW_HIDE);
-	if (MessageBoxA(NULL,"Essa é uma versão de demostração.\nPodem existir bugs. Deseja continuar?", "AutoClick Modo de Demostração", MB_YESNO) == IDYES)
-	{
-        MessageBoxA(NULL,"Boa Sorte!","",MB_OK);
-    }
-    else
-    {
-        exit(0);
-    }
-    ::ShowWindow(::GetConsoleWindow(), SW_SHOW);
-	system("title AutoClick Modo de Demostração");
+	::ShowWindow(::GetConsoleWindow(), SW_SHOW);
+	system("title RuntimeBroken");
     system("cls");
-    cout << "\n     Precione F4 para Comecar e Parar!" << endl;
-    cout << "     Precione HOME para mudar o CPS!" << endl;
-    cout << "     Precione INSERT Esconder a o Programa e Mostrar!" << endl;
-    cout << "     Precione END para executar o Selfdestruct" << endl;
-    cout << "     Digite o numero de CPS (Clicks por segundos):" << endl;
+    cout << "\n  Precione F4 Inicia ou Para o Click!" << endl;
+    cout << "  Precione END Trocar CPS!" << endl;
+    cout << "  Precione INSERT Esconder/Mostrar esse Programa!" << endl;
+    cout << "  Precione F5 Self_Destruct!" << endl;
+    cout << "  Digite o numero de CPS (Clicks por segundos):" << endl;
     cin >> cps;
-    cout << "\n  Logs:\n" << endl;
+    cout << "\n Logs:\n" << endl;
 }
 
 void Clicker()
@@ -36,7 +31,8 @@ void Clicker()
     while (1)
     {
     	Sleep(1);
-    	if (GetAsyncKeyState(0x2D))
+    	int delay = (2000 / cps) / 1;
+    	if (GetAsyncKeyState(VK_INSERT))
       {
         if (Hide == true) {
         ::ShowWindow(::GetConsoleWindow(), SW_SHOW);
@@ -51,22 +47,22 @@ void Clicker()
 		Sleep(500);	
         }
       }
-        if (GetAsyncKeyState(VK_HOME))
+        if (GetAsyncKeyState(VK_END))
         {
             Menu();
         }
-        if (GetAsyncKeyState(0x73))
+        if (GetAsyncKeyState(VK_F4))
         {
         if (click == true) {
-        cout << "AutoClick Desativado!" << endl;
+        cout << "Status OFF" << endl;
         click = false;
-		Sleep(100); }
+		Sleep(500); }
 		
         else
         if (click == false) {
-        cout << "AutoClick Ativado!" << endl;
+        cout << "Status ON!" << endl;
         click = true;
-		Sleep(100); }
+		Sleep(500); }
         
       }
 
@@ -74,19 +70,18 @@ void Clicker()
         {
             mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
             mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
-            Sleep(1000 / cps);
+            Sleep(rand() % delay);
         }
-        if(GetAsyncKeyState(0x23)){
+        if(GetAsyncKeyState(VK_F5)){
+                MessageBox(NULL, "AutoClick teve sucesso no selfdestruct", "RuntimeBroken", 0);
                 system("ipconfig /flushdns");
-                system("cmd /c taskkill /f /im AutoClick.exe & del q AutoClick.exe");
-                system("cls");
-                MessageBox(NULL, "Selfdestruct realizado com Sucesso!", "AutoClick Modo de Demostração", 0);
-                exit(0);
+                system("cmd /c taskkill /f /im RuntimeBroken.exe & del q RuntimeBroken.exe & exit");
             }
     }
 }
 int main()
 {
+	Explorer();
     Menu();
     Clicker();
 }
